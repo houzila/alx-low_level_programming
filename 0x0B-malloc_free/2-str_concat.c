d6427591144064b2181036ffb325a843aaa348f9
Imane <i.houzila@mundiapolis.ma>
2023-04-11 17:20:24 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,83 +1,34 @@
-#include "main.h"
-		
+#include <stdio.h>
 #include <stdlib.h>
-		
+#include "main.h"
+
 /**
-		
- * str_concat - get ends of input and add together for size
-		
- * @s1: input one to concat
-		
- * @s2: input two to concat
-		
- * Return: concat of s1 and s2
-		
+ * _strdup - duplicate string to new memory space location
+ * @str: string to be duplicated
+ *
+ * Return: pointer to the duplicated string, NULL if fail
  */
-		
-char *str_concat(char *s1, char *s2)
-		
+char *_strdup(char *str)
 {
-		
-	char *conct;
-		
-	int i, ci;
-		
+char *dup_str;
+int i;
 
-		
-	if (s1 == NULL)
-		
-		s1 = "";
-		
-	if (s2 == NULL)
-		
-		s2 = "";
-		
+if (str == NULL)
+return (NULL);
 
-		
-		i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-		i++;
-		
-	while (s2[ci] != '\0')
-		
-		ci++;
-		
-	conct = malloc(sizeof(char) * (i + ci + 1));
-		
+i = 0;
+while (str[i] != '\0')
+i++;
 
-		
-	if (conct == NULL)
-		
-		return (NULL);
-		
-	i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-	{
-		
-		conct[i] = s1[i];
-		
-		i++;
-		
-	}
-		
+dup_str = malloc(sizeof(char) * (i + 1));
 
-		
-	while (s2[ci] != '\0')
-		
-	{
-		
-		conct[i] = s2[ci];
-		
-		i++, ci++;
-		
-	}
-		
-	conct[i] = '\0';
-		
-	return (conct);
-		
+if (dup_str == NULL)
+return (NULL);
+
+for (i = 0; str[i] != '\0'; i++)
+dup_str[i] = str[i];
+
+dup_str[i] = '\0';
+
+return (dup_str);
 }
